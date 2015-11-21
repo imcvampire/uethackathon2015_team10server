f78cf26f85037fe9f35ab3f423fe9e6fdd42441a
Dzung Nguyen <dungtoanhoc@yahoo.com.vn>
2015-11-22 01:46:44 +0700

    Add list(error)

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,45 +2,99 @@
 
 @section('styles')
 	<link rel="stylesheet" type="text/css" href="/css/profile.css">
+	<link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
 	<style>
 		h3 {
 			text-align: center; 
 			color: red;
 		}
 	</style>
+	<meta id="token" name="token" value="{{ csrf_token() }}">
+	<meta id="s_id" name="s_id" value="subject_id">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.vertical-tabs.min.css">
 @stop
 
 @section('content')
+	@include('headers.header2')
 	<div class="container">
 	    <div class="row">
-	        <span class="col-xs-12 text-center text-info text-capitalize">{{ $user->name }}</span>
 
 	        <div class="row" id="info">
-	            <img class="col-sm-3 img-responsive img-thumbnail" src="http://www.gravatar.com/avatar/{{ md5(strtolower(trim($user->email))) }}">
+	            <img class="col-xs-4 col-xs-offset-4 img-responsive img-thumbnail" src="http://www.gravatar.com/avatar/{{ md5(strtolower(trim($user->email))) }}" style="width: 200px">
 
-	            <div class="col-sm-9">
-	                <p>Tên: {{ $user->name }}</p>
+	            <div class="col-xs-4 col-xs-offset-4">
+	                <p><b>Tên</b>: {{ $user->name }}</p>
 
-	                <p>Email: {{ $user->email }}</p>
+	                <p><b>Email</b>: {{ $user->email }}</p>
 
-	                <p>Điểm số: {{ $user->score }}</p>
+	                <p><b>Điểm số</b>: {{ $user->score }}</p>
 
 	            </div> <!-- .col-sm-9 -->
 	        </div> <!-- .row #info -->
-	        <div id="subject">
-	            <div class="table-responsive">
-	                <table class="table table-hover table-bordered">
-	                    <thead>
-		                    <tr>
-		                        <td><b>Môn học đã hoàn thành</b></td>
-		                    </tr>
-	                    </thead>
-	                </table>
-	                @if ($subjects1->count() == 0)
+
+
+    
+	<div class="container" id="tabs">
+		<ul class="nav nav-tabs">
+		  <li><a data-toggle="tab" href="#unfinished">Đang học</a></li>
+		  <li><a data-toggle="tab" href="#finished">Đã hoàn thành</a></li>
+		  <li><a data-toggle="tab" href="#contribute">Đóng góp</a></li>
+		  <li><a data-toggle="tab" href="#wall">Lời nhắn</a></li>
+		</ul>
+		<div class="tab-content">
+		  <div id="unfinished" class="tab-pane fade in active">
+		    <ul class="list-group">
+	                    @if ($subjects0->count() == 0)
+	                	<h3>Bạn không đang học môn học nào</h3>
+	                @else
+		                @foreach ($subjects0 as $subject)
+		                	<h3><a href="/subjects/">{{ $subject->name }}</a></h3>
+		                	<div class="row">
+		                	<ul class="list-group">
+		                		<li class="list-group-item col-xs-4">
+		                			<h4>Sách đã chọn </h4>
+		                			<ul class="list-group">
+		                				@foreach ($books as $book)
+		                					@if ($book->subject == $subject)
+		                						<li class="list-group-item">{{ $book->name }}</li>
+		                					@endif
+		                				@endforeach
+		                			</ul>
+		                		</li>
+		                		<li class="list-group-item col-xs-4">
+		                			<h4>Website đã chọn</h4>
+		                			<ul class="list-group">
+		                				@foreach ($websites as $website)
+		                					@if ($website->subject == $subject)
+		                						<li class="list-group-item"><a href="{{ $website->link }}"{{ $website->name }}</a></li>
+		                					@endif
+		                				@endforeach
+		                			</ul>
+		                		</li>
+		                		<li class="list-group-item col-xs-4">
+		                			<h4>Người đã chọn</h4>
+		                			<ul class="list-group">
+		                				@foreach($persons as $person)
+		                					@if ($person->subject == $subject)
+		                						<li class="list-group-item">
+		                							<a href="{{ $person->link }}">{{ $person->name }}</a>
+		                						</li>
+		                					@endif
+		                				@endforeach
+		                			</ul>
+		                		</li>
+		                	</ul>
+		                	</div>
+		                @endforeach	
+	                @endif
+		  </div>
+		  <div id="finished" class="tab-pane fade">
+		     @if ($subjects1->count() == 0)
 	                	<h3>Bạn chưa hoàn thành môn học nào</h3>
 	                @else
 		                @foreach ($subjects1 as $subject)
-		                	<h3><a href="/subjects/{{ $subject->id }}">{{ $subject->name }}</a></h3>
+		                	<h3><a href="/subjects/"></a></h3>
 		                	<ul class="list-group">
 		                		<li class="list-group-item">
 		                			<h4>Sách đã chọn </h4>
@@ -77,107 +131,63 @@
 		                	</ul>
 		                @endforeach	
 	                @endif
-	            </div> <!-- .table-responsive -->
-	            <div class="table-responsive">
-	                <table class="table table-hover table-bordered">
-	                    <thead>
-	                    <tr>
-	                        <td><b>Môn học đang học</b></td>
-	                    </tr>
-	                    </thead>
-	                </table>
-	                @if ($subjects0->count() == 0)
-	                	<h3>Bạn không đang học môn học nào</h3>
-	                @else
-		                @foreach ($subjects0 as $subject)
-		                	<h3><a href="/subjects/{{ $subject->id }}">{{ $subject->name }}</a></h3>
-		                	<ul class="list-group">
-		                		<li class="list-group-item">
-		                			<h4>Sách đã chọn </h4>
-		                			<ul class="list-group">
-		                				@foreach ($books as $book)
-		                					@if ($book->subject == $subject)
-		                						<input type="checkbox" name="book_select" id="book_select">
-		                						<li class="list-group-item">{{ $book->name }}</li>
-		                					@endif
-		                				@endforeach
-		                				@foreach ($books as $book)
-		                						<input type="checkbox" name="book_select" id="book_select">
-		                						<li class="list-group-item">{{ $book->name }}</li>
-		                				@endforeach
-		                			</ul>
-		                		</li>
-		                		<li class="list-group-item">
-		                			<h4>Website đã chọn</h4>
-		                			<ul class="list-group">
-		                				@foreach ($websites as $website)
-		                					@if ($website->subject == $subject)
-		                						<input type="checkbox" name="website_select" id="website_select" >
-		                						<li class="list-group-item"><a href="{{ $website->link }}"{{ $website->name }}</a></li>
-		                					@endif
-		                				@endforeach
-		                				@foreach ($websites as $website)
-		                						<input type="checkbox" name="website_select" id="website_select">
-		                						<li class="list-group-item"><a href="{{ $website->link }}">{{ $website->name }}</a></li>
-		                				@endforeach
-		                			</ul>
-		                		</li>
-		                		<li class="list-group-item">
-		                			<h4>Người đã chọn</h4>
-		                			<ul class="list-group">
-		                				@foreach($persons as $person)
-		                					@if ($person->subject == $subject)
-		                						<li class="list-group-item">
-		                							<a href="{{ $person->link }}">{{ $person->name }}</a>
-		                						</li>
-		                					@endif
-		                				@endforeach
-		                			</ul>
-		                		</li>
-		                	</ul>
-		                @endforeach	
-	                @endif
-	            </div> <!-- .table-responsive -->
-	            <div class="table-responsive">
-	                <table class="table table-hover table-bordered">
-	                    <thead>
-		                    <tr>
-		                        <td>Đóng góp</td>
-		                    </tr>
-	                    </thead>
-	                </table>
+		  </div>
+		  <div id="contribute" class="tab-pane fade">
+		  		<div class="col-xs-3"> <!-- required for floating -->
+			    <!-- Nav tabs -->
+			    <ul class="nav nav-tabs tabs-left">
+			      <li class="active"><a href="#subjects" data-toggle="tab">Môn học</a></li>
+			      <li><a href="#books" data-toggle="tab">Sách</a></li>
+			      <li><a href="#websites" data-toggle="tab">Websites</a></li>
+			      <li><a href="#persons" data-toggle="tab">Chuyên gia</a></li>
+			    </ul>
+			</div>
 
-             		<ul class="list-group">
+			<div class="col-xs-9">
+			    <!-- Tab panes -->
+			    <div class="tab-content">
+			      <div class="tab-pane active" id="subjects">
+			      <ul class="list-group">
                     	<h3>Môn học</h3>
                     	@foreach ($user->created_subjects as $subject)
-                    		<li class="list-group-item"><a href="/subjects/{{ $subject->id }}"></a>{{ $subject->name }}</a></li>
+                    		<li class="list-group-item"><a href="/subjects/{{ $subject->name }}"></a>{{ $subject->name }}</a></li>
                     	@endforeach
-                    </ul>
-
-                    <ul class="list-group">
+                    </ul></div>
+			      <div class="tab-pane" id="books">
+			      	<ul class="list-group">
                     	<h3>Sách</h3>
                     	@foreach ($user->created_books as $book)
                     		<li class="list-group-item">{{ $book->name }}</li>
                     	@endforeach
                     </ul>
-
-                    <ul class="list-group">
+			      </div>
+			      <div class="tab-pane" id="websites">
+			      	<ul class="list-group">
                     	<h3>Websites</h3>
                     	@foreach ($user->created_websites as $website)
                     		<li class="list-group-item"><a href="{{ $website->link }}">{{ $website->name }}</a></li>
                     	@endforeach
                     </ul>
-
-                    <ul class="list-group">
+			      </div>
+			      <div class="tab-pane" id="persons">
+			      	<ul class="list-group">
                     	<h3>Chuyên gia</h3>
                     	@foreach ($user->created_persons as $person)
                     		<li class="list-group-item"><a href="{{ $person->link }}">{{ $person->name }}</a></li>
                     	@endforeach
                     </ul>
-	            </div> <!-- .table-responsive -->
-	        </div> <!-- #subject -->
-	    </div> <!-- .row -->
-	</div> <!-- container -->
+			      </div>
+			    </div>
+			</div>  
+		  </div>
+		  
+		  <div id="wall" class="tab-pane fade">
+		  	@include('subjects.disqus')
+		  </div>
+		</div>
+		</div>
+
+	
 @stop
 
 @section('scripts')
