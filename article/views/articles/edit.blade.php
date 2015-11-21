968e2755c8cc6e20c4c151d01a0f08954e759693
hongnguyenhuu96 <hongnguyenhuu96@gmail.com>
2015-11-21 15:27:51 +0700

    add articles

---
+++ article/views/articles/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('articles.boostrap')
+
+@section('content')
+    <h1>Edit: {!! $article->title !!}</h1>
+    {!! Form::model($article,['method' => 'PATCH', 'action' => ['ArticlesController@update', $article->id]]) !!}
+    @include('articles.form',['submitButtonText' => 'Edit']);
+    {!! Form::close() !!}
+    {!! Form::open([
+        'method' => 'DELETE',
+        'action' => ['ArticlesController@destroy',$article->id],
+    ]) !!}
+
+    <div class="form-group">
+        {!! Form::submit('delete', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
+    {!! Form::close() !!}
+    {{--@include('errors.list');--}}
+@stop
